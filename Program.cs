e5b9f459eee403fef0d0e33d2ae7182ac39d6a65
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 19:35:20 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.Clear();
+int i, c;
+int N, P, AP;
+
+c = 2;
+N = 1;
+P = 1;
+
+for(i = 1; i < c;)
+{
+c = c + 3;
+AP = i;
+
+
+Thread.Sleep(30000);
+Console.Clear();
+Console.WriteLine(@$"--------------GERADOR DE SENHAS-------------- ");
+Console.WriteLine("AP" + AP);
+
+Thread.Sleep(15000);
+Console.Clear();
+Console.WriteLine(@$"--------------GERADOR DE SENHAS-------------- ");
+Console.WriteLine("P" + P);
+Thread.Sleep(15000);
+AP = AP + 1;
+Console.Clear();
+Console.WriteLine(@$"--------------GERADOR DE SENHAS-------------- ");
+Console.WriteLine("AP" + AP);
+Thread.Sleep(30000);
+P = P + 1;
+AP = AP + 1;
+Console.Clear();
+Console.WriteLine(@$"--------------GERADOR DE SENHAS-------------- ");
+Console.WriteLine("N" + N);
+Console.WriteLine("AP" + AP);
+N = N + 1;
+i = i + 3;
+}
+
